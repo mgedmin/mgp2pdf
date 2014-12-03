42f5f8d233049883a9039d86044a6838171b415f
Marius Gedminas <marius@gedmin.as>
2014-12-03 19:18:56 +0200

    Start tests for error handling
    Adds a dependency on mock.

--- tests.py
+++ tests.py
@@ -2,7 +2,10 @@ import doctest
 import unittest
 from cStringIO import StringIO
 
+import mock
+
 import mgp2pdf
+
 
 sample_mgp = """\
 # This is an MGP file
@@ -17,12 +20,25 @@ World!
 """
 
 
-class Tests(unittest.TestCase):
+class SmokeTests(unittest.TestCase):
 
     def test_conversion(self):
         p = mgp2pdf.Presentation(StringIO(sample_mgp), title="Sample")
         pdf = StringIO()
         p.makePDF(pdf)
+
+
+class TestImage(unittest.TestCase):
+
+    @mock.patch('mgp2pdf.ImageReader')
+    @mock.patch('mgp2pdf.log')
+    def test_drawOn_error_handling(self, mock_log, mock_ImageReader):
+        mock_ImageReader().getSize.return_value = 50, 75
+        canvas = mock.Mock()
+        canvas.drawImage.side_effect = IOError('no such file')
+        img = mgp2pdf.Image('image.png')
+        x, y = img.drawOn(canvas, 10, 20, 100, 200)
+        self.assertEqual((x, y), (60, 20))
 
 
 def test_suite():
