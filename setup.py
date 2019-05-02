d1e54c1e45d8e0b74c957942939daa6764f29395
Marius Gedminas <marius@gedmin.as>
2019-05-02 17:22:05 +0300

    Add PyPy support

--- setup.py
+++ setup.py
@@ -26,6 +26,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
     ],
     license='GPL',
 
